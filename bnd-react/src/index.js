dc9487e3d9b754009834ebb5763430c55c2c0a7c
JonathanDimercurio <jonathandimercurio@mymail.mines.edu>
2021-10-04 00:41:09 -0600

    Additional file changes

--- bnd-react/src/index.js
+++ bnd-react/src/index.js
@@ -1,12 +1,16 @@
+/*  Author: Jonathan Dimercurio     Date:
+ *  Description: 
+    */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/**     This is the first object loaded by the browser.
+ *     This is jsx code, which will be converted into js.
+    * */
 
+      //.render( <what we are render>, <where it is> )
+ReactDOM.render(<App />, document.getElementById('root'));
