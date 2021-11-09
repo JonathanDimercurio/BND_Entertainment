b38892e4b6280f1151997d690ade6a2a8101e005
Danny Rosen <danielrosen29@gmail.com>
2021-11-09 12:02:49 -0700

    Added fill on click

--- src/components/game/Gameboard.js
+++ src/components/game/Gameboard.js
@@ -4,6 +4,8 @@ import "./Gameboard.modal.css"
 
 export default class Gameboard extends React.Component
 {
+
+
     m=16;
     n=10;
     
@@ -32,11 +34,10 @@ export default class Gameboard extends React.Component
             overflow: "hidden",
         }}
         >
-            {/* {this.grid.map((row)=>{row.map(index=>{return(<>index</>)})})} */}
             {this.grid.map((row,key)=>{
-                row.map((index,i_key)=>{ return index})
+                row.map( ( index,i_key )=> { return index } )
                 return row})}
-            {/*<img src={this.props.mapName} className="img" />*/}
+            <img src={this.props.mapName} className="img" />
         </div> 
         );
     }
@@ -45,24 +46,49 @@ export default class Gameboard extends React.Component
 
 class Square extends React.Component 
 {
+
     constructor(props)
     {
         super(props)
+
+        this.state = {
+            isSelected: false
+        }
     }
     
+    
+
     onclick = () =>{
-        console.log("clicked on me");
+        if (this.state.isSelected){
+            this.setState({
+                isSelected: false
+              })
+        } else {
+        this.setState({
+            isSelected: true
+          })
+        }
     }
 
     render(){
         return( 
 
+            
             this.props.image ?
             <img src={this.props.image}  onClick={this.onclick} style={{position: "relative", display:"inline-block", width: 100/(this.props.m)+"%", height: 93/(this.props.n)+"%", border: "solid 1px black"}}/>
                 :
-            <div  onClick={this.onclick} style={{position: "relative",display:"inline-block", width: 100/(this.props.m)+"%", height: 93/(this.props.n)+"%", border: "solid 1px black"}} />
-           
-    
+            <div  onClick={this.onclick} style={{overflow: "hidden", position: "relative",display:"inline-block", width: 100/(this.props.m)+"%", height: 93/(this.props.n)+"%", border: "solid 1px black"}}>
+                <div>
+                    {this.state.isSelected ? 
+                        <img src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE4MDAzNDEwNzk1MjAyMDYy/jake-gyllenhaal-16242387-1-402.jpg"/>
+                        :
+                        ""
+                    }
+                    </div>
+            </div>
+                
+            
+            
         );
     }
 }
